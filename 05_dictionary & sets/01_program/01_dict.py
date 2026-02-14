a240a8fce0291d839ffa082cc2d4045dd4a4a01a
Sourabh037 <bhandarisourabh2003@gmail.com>
2026-02-14 20:53:00 +0530

    new

---
+++ 05_dictionary & sets/01_program/01_dict.py
@@ -0,0 +1,14 @@
+# to define a dictionary we use {} and it's unordered
+dictionary is mutable
+
+marks =  {"sourabh" : 70,
+           "rohan" : 20,
+           "shubham" : 100
+         }
+
+# in dictionary value are store in two pair one is key other value 
+# there can't be two duplicate keys and it is indexed means
+# it not going to check one by one once given a key it will directly jump there
+
+print(marks, type(marks))
+print(marks["rohan"])
