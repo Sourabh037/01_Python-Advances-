1c5d3d550600820ed989b4a9cb522b85ba644b4b
Sourabh037 <bhandarisourabh2003@gmail.com>
2026-04-21 19:54:06 +0530

    later

--- 10_oops/program/basic.py
+++ 10_oops/program/basic.py
@@ -1,1 +1,11 @@
- 
+class employee:
+    #  these are class attribute
+    name = "harry"  #----------------------
+    language = "py"         #              |
+    salary = 120000         #              |
+                             #             |
+sourabh = employee()          #            |
+# name is object instance attribute        |
+sourabh.name = "sourabh"       #           |
+# first will always be instance attribute--| 
+print(sourabh.name, sourabh.language, sourabh.salary)
